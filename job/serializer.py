8e6e7fc5c9d4387a1dd4c88e7f8379972237e43f
Pranshu1902 <pranshu1902@gmail.com>
2024-06-19 22:19:18 +0530

    refactor user serializer

--- job/serializer.py
+++ job/serializer.py
@@ -4,17 +4,10 @@ from .models import Job, JobStatusUpdate, JobComment, Company
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-
     class Meta:
         model = User
         fields = ["id", "username", "email", "password", "first_name", "last_name"]
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"], password=validated_data["password"]
-        )
-        return user
+        read_only_fields = ["password"]
 
 
 class JobStatusUpdateSerializer(serializers.ModelSerializer):
