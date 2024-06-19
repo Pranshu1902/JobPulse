ca4fd8e903fa26164ac950c38bb14520c3efaa6a
Pranshu1902 <pranshu1902@gmail.com>
2024-06-19 21:02:03 +0530

    send user details in serializer

--- job/serializer.py
+++ job/serializer.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password"]
+        fields = ["id", "username", "email", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
